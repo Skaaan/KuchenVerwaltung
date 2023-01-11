27d3dd5379ba26761c4b0c17980714da47f83885
root <root@DESKTOP-BRNI6R8.localdomain>
2023-01-11 19:20:10 +0100

    abstract is back

--- src/ExecuteGUI.java
+++ src/ExecuteGUI.java
@@ -2,7 +2,7 @@ import gui.GUI;
 import javafx.application.Application;
 
 
-public class ExecuteGUI extends Application {
+public abstract class ExecuteGUI extends Application {
 
     public static void main(String[] args) {
         Application.launch(GUI.class, args);
